e63eceec7137bb1bb87fdc35f88d86c12dfdec69
Gokul <pgokulmca@gmail.com>
2022-05-14 21:41:40 +0530

    Grape API

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module Blog
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.paths.add File.join('app', 'api'), glob: File.join('**', '*.rb')
+    config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
   end
 end
